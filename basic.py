59baf901293ea86281db70726cb4b2afef6d52ae
Diamond Dai <diamond.dai60@gmail.com>
2024-05-24 00:11:40 -0600

    Reconstruct the css and js for Flask framework

--- basic.py
+++ basic.py
@@ -6,6 +6,21 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("home.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/email")
+def email():
+    return render_template("email.html")
+
+@app.route("/quiz")
+def quiz():
+    return render_template("quiz.html")
 
 if __name__ == "__main__" :
     app.run()
---
+++ static/script/home.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $('#head1').fadeIn(1000);
+    $('#diamondImg').delay(1000).fadeIn(1000);
+    $('#head3').delay(2000).fadeIn(1000);
+});
